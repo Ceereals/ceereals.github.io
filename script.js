7358d4687e7aff9cb48b07b2a01508576dc2f5aa
Ceereals <romoli16.rr@gmail.com>
2017-12-15 19:36:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function move(b,a) {
+function move(b,a,c) {
     var elem = b;
       var width = 1;
       var id = setInterval(frame, 10);
@@ -8,15 +8,16 @@ function move(b,a) {
           } else {
               width++;
               elem.css("width", width + '%');
+              if(c)elem.text(width+"%");
           }
       }
   }
   $(document).ready(() => {
-    move($("#cpp"),60);
-    move($("#html"),50);
-    move($("#js"),40);
-    move($("#css"),20);
-    move($("#it"),100);
-    move($("#en"),55);    
+    move($("#cpp"),60,true);
+    move($("#html"),50,true);
+    move($("#js"),40,true);
+    move($("#css"),20,true);
+    move($("#it"),100,false);
+    move($("#en"),55,false);    
     
 });
